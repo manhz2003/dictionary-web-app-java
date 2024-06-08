f7501194e9b4e425c714831a5cdec4783c6709e1
manhz2003 <manhthenguyen2003@gmail.com>
2024-06-08 12:18:12 +0700

    create server

---
+++ server/src/test/java/com/example/dictionaryapplication/DictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dictionaryapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
